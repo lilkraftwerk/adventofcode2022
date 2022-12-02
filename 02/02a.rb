7e4419370697486b6a8e6987858c83afc2eb06d5
Lucas Mathis <lucasquinnmathis@gmail.;com>
2022-12-02 10:10:20 +0100

    Day two done

---
+++ 02/02a.rb
@@ -0,0 +1,46 @@
+matches = File.read("./02/input.txt").split("\n").map { _1.split(" ") }
+
+class RockPaperScissor
+  def initialize(char)
+    @char = char
+  end
+
+  def type
+    return :rock if ["A", "X"].include?(@char)
+    return :paper if ["B", "Y"].include?(@char)
+    return :scissors if ["C", "Z"].include?(@char)
+  end
+end
+
+def score_match(match)
+  rock_value = 1
+  paper_value = 2
+  scissors_value = 3
+
+  loss = 0
+  tie = 3
+  win = 6
+
+  opponent = RockPaperScissor.new(match[0])
+  me = RockPaperScissor.new(match[1])
+
+  if me.type == :rock
+    return rock_value + loss if opponent.type == :paper
+    return rock_value + tie if opponent.type == :rock
+    return rock_value + win if opponent.type == :scissors
+  end
+
+  if me.type == :paper
+    return paper_value + loss if opponent.type == :scissors
+    return paper_value + tie if opponent.type == :paper
+    return paper_value + win if opponent.type == :rock
+  end
+
+  if me.type == :scissors
+    return scissors_value + loss if opponent.type == :rock
+    return scissors_value + tie if opponent.type == :scissors
+    return scissors_value + win if opponent.type == :paper
+  end
+end
+
+print matches.map { |match| score_match(match) }.sum == 10718
---
+++ 02/02b.rb
@@ -0,0 +1,50 @@
+matches = File.read("./02/input.txt").split("\n").map { _1.split(" ") }
+
+class RockPaperScissor
+  def initialize(char)
+    @char = char
+  end
+
+  def type
+    return :rock if ["A", "X"].include?(@char)
+    return :paper if ["B", "Y"].include?(@char)
+    return :scissors if ["C", "Z"].include?(@char)
+  end
+end
+
+def score_match(match)
+  i_lose = "X" == match[1]
+  i_tie = "Y" == match[1]
+  i_win = "Z" == match[1]
+
+  rock_value = 1
+  paper_value = 2
+  scissors_value = 3
+
+  loss = 0
+  tie = 3
+  win = 6
+
+  opponent = RockPaperScissor.new(match[0])
+
+  if opponent.type == :rock
+    return scissors_value + loss if i_lose
+    return rock_value + tie if i_tie
+    return paper_value + win if i_win
+  end
+
+
+  if opponent.type == :scissors
+    return paper_value + loss if i_lose
+    return scissors_value + tie if i_tie
+    return rock_value + win if i_win
+  end
+
+  if opponent.type == :paper
+    return rock_value + loss if i_lose
+    return paper_value + tie if i_tie
+    return scissors_value + win if i_win
+  end
+end
+
+print matches.map { |match| score_match(match) }.sum == 14652
